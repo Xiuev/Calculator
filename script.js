7a83e5ab39bab1d7f668e6826798b57ee752e8b6
Carl Laguerre <gglaguerre1@gmail.com>
2025-01-09 23:39:16 -0500

    reseting code

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ console.log("hi");
 let numPad = Array.from(document.querySelectorAll(".number"));
 let operationPad = Array.from(document.querySelectorAll(".operator"));
 let itemDisplay = document.querySelector("#clcDisplayNums");
-let operatorDisplay = document.querySelector("#clcDisplayOperator");
 let itemDisplayTwo = document.querySelector("#clcDisplaySecondNums");
+let operatorDisplay = document.querySelector("#clcDisplayOperator");
 let calcDisplayParent = document.querySelector(".calc-display");
-//have a "screen" portion be an array? so for example
+
 //operation FUCNTIONS
 function Add(...values) {
   let sumOfValues = values.reduce((accum, currentVal) => {
@@ -35,53 +35,3 @@ function Divide(...values) {
   });
   return divideOfValues;
 }
-
-function populateScreen() {
-  let currentNumber = 0;
-  let digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-  let opSigns = ["+", "-", "X", "/"];
-  let screenArray = [];
-  //console.log(digits);
-
-  for (let i = 0; i < operationPad.length; i++) {
-    operationPad[i].addEventListener("click", () => {
-      operatorDisplay.textContent = opSigns[i];
-      //add operations to screenArray
-      userOperationInput = operatorDisplay.textContent;
-      if (currentNumber !== "" && !isNaN(currentNumber)) {
-        screenArray.push(currentNumber);
-        currentNumber = 0;
-        itemDisplay.textContent = "";
-        //screenArray.push(userOperationInput);
-      }
-
-      console.log(screenArray);
-      console.log(currentNumber);
-    });
-  }
-
-  for (let i = 0; i < numPad.length; i++) {
-    numPad[i].addEventListener("click", () => {
-      //convert to int the textContent node specefially
-      //itemDisplay.textContent = digits[i];
-      if (itemDisplay.textContent === "0") {
-        // check if current value before appending? --> remove it?
-        itemDisplay.textContent = "";
-      }
-
-      let number = document.createTextNode(digits[i]);
-      itemDisplay.appendChild(number);
-      let userDigitInput = parseInt(itemDisplay.textContent);
-      currentNumber = userDigitInput;
-      //screenArray.push(userDigitInput);
-      //console.log(screenArray);
-      //console.log(currentNumber);
-    });
-  }
-  //call operate function somewhere
-}
-function operate() {}
-
-populateScreen();
-
-//console.log(itemDisplay.textContent);
