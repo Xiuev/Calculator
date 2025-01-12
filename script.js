48aeb61ccc27b2ee552d7abcc9336de3864ec73f
Carl Laguerre <gglaguerre1@gmail.com>
2025-01-11 23:01:05 -0500

    create operate function

--- script.js
+++ script.js
@@ -6,8 +6,34 @@ let itemDisplayTwo = document.querySelector("#clcDisplaySecondNums");
 let operatorDisplay = document.querySelector("#clcDisplayOperator");
 let calcDisplayParent = document.querySelector(".calc-display");
 
+//operator varibles
+let num1 = 0;
+let num2 = 0;
+let operation = false;
+//display functions
+function populateDisplay() {}
 //operation FUCNTIONS
-function Add(...values) {
+function operate(operation, num1, num2) {
+  //condition to check operation
+  //use switch
+  switch (operation) {
+    case add:
+      return add(num1, num2);
+      break;
+    case subtract:
+      return subtract(num1, num2);
+      break;
+    case multiply:
+      return multiply(num1, num2);
+      break;
+    case divide:
+      return divide(num1, num2);
+      break;
+    default:
+      console.log("invalid!");
+  }
+}
+function add(...values) {
   let sumOfValues = values.reduce((accum, currentVal) => {
     return accum + currentVal;
   }, 0);
@@ -15,23 +41,25 @@ function Add(...values) {
   return sumOfValues;
 }
 
-function Subtract(...values) {
+function subtract(...values) {
   let diffOfValues = values.reduce((acum, currentVal) => {
     return acum - currentVal;
   });
   return diffOfValues;
 }
 
-function Multiply(...values) {
+function multiply(...values) {
   let prodOfValues = values.reduce((acum, currentVal) => {
     return acum * currentVal;
   });
   return prodOfValues;
 }
 
-function Divide(...values) {
+function divide(...values) {
   let divideOfValues = values.reduce((accum, currentValue) => {
     return accum / currentValue;
   });
   return divideOfValues;
 }
+
+console.log(operate(add, 1, 2));
