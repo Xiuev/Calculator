b2e5dd0c244dd5bdd254e926008d07833223767a
Carl Laguerre <gglaguerre1@gmail.com>
2025-01-15 23:27:28 -0500

    Updated function to properly create text node to append

--- script.js
+++ script.js
@@ -11,7 +11,51 @@ let num1 = 0;
 let num2 = 0;
 let operation = false;
 //display functions
-function populateDisplay() {}
+function populateDisplay() {
+  let displayVar1 = 0;
+  let displayVar2 = 0;
+  //operateDisplay();
+  function operateDisplay() {
+    for (let i = 0; i < operationPad.length; i++) {
+      operationPad[i].addEventListener("click", function () {
+        switch (operationPad[i].textContent) {
+          case "+":
+            operatorDisplay.textContent = "+";
+            break;
+          case "-":
+            operatorDisplay.textContent = "-";
+            break;
+          case "x":
+            operatorDisplay.textContent = "x";
+            break;
+          case "/":
+            operatorDisplay.textContent = "/";
+            break;
+          case "=":
+            operatorDisplay.textContent = "=";
+            break;
+
+          case "C":
+            operatorDisplay.textContent = "";
+            itemDisplay.textContent = "0";
+            itemDisplayTwo.textContent = "";
+            break;
+        }
+      });
+    }
+  }
+  for (let i = 0; i < numPad.length; i++) {
+    numPad[i].addEventListener("click", function () {
+      if (operatorDisplay.textContent === "") {
+        //append text content to parent
+        //try creating node containing content i want to append
+        //itemDisplay.textContent = "";
+        let newTextNode = document.createTextNode(numPad[i].textContent);
+        itemDisplay.appendChild(newTextNode);
+      }
+    });
+  }
+}
 //operation FUCNTIONS
 function operate(operation, num1, num2) {
   //condition to check operation
@@ -62,4 +106,6 @@ function divide(...values) {
   return divideOfValues;
 }
 
-console.log(operate(add, 1, 2));
+//console.log(operate(add, 1, 2));
+populateDisplay();
+console.log(operationPad[1].textContent);
